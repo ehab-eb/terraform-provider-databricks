10ee7c10a07f7ea6f6be9c149aae7367a85ddff2
ehab-eb <ehabelbadrawy@live.com>
2023-11-19 15:25:47 +0100

    Checkpoint

--- aws/data_instance_profiles.go
+++ aws/data_instance_profiles.go
@@ -2,6 +2,9 @@ package aws
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/databricks/databricks-sdk-go"
@@ -9,30 +12,55 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+func matchesIP(s *instanceProfileData, re *regexp.Regexp, field string) bool {
+	var x map[string]interface{}
+	m, _ := json.Marshal(s)
+	_ = json.Unmarshal(m, &x)
+	f := fmt.Sprint(x[field])
+	return re.Match([]byte(regexp.QuoteMeta(f)))
+}
+
+type instanceProfileData struct {
+	Name    string `json:"name,omitempty" tf:"computed"`
+	Arn     string `json:"arn,omitempty" tf:"computed"`
+	RoleArn string `json:"role_arn,omitempty" tf:"computed"`
+	IsMeta  bool   `json:"is_meta,omitempty" tf:"computed"`
+}
+
 func DataSourceInstanceProfiles() *schema.Resource {
-	type instanceProfileData struct {
-		Name    string `json:"name,omitempty" tf:"computed"`
-		Arn     string `json:"arn,omitempty" tf:"computed"`
-		RoleArn string `json:"role_arn,omitempty" tf:"computed"`
-		IsMeta  bool   `json:"is_meta,omitempty" tf:"computed"`
+	type instanceProfileFilter struct {
+		Name    string `json:"name,omitempty" tf:"required"`
+		Pattern string `json:"pattern,omitempty" tf:"required"`
 	}
 	return common.WorkspaceData(func(ctx context.Context, data *struct {
 		InstanceProfiles []instanceProfileData `json:"instance_profiles,omitempty" tf:"computed"`
+		Filter           instanceProfileFilter `json:"filter,omitempty"`
 	}, w *databricks.WorkspaceClient) error {
 		instanceProfiles, err := w.InstanceProfiles.ListAll(ctx)
 		if err != nil {
 			return err
 		}
+
+		var InstanceProfilesData []instanceProfileData
 		for _, v := range instanceProfiles {
 			arnSlices := strings.Split(v.InstanceProfileArn, "/")
 			name := arnSlices[len(arnSlices)-1]
-			data.InstanceProfiles = append(data.InstanceProfiles, instanceProfileData{
+			ipData := instanceProfileData{
 				Name:    name,
 				Arn:     v.InstanceProfileArn,
 				RoleArn: v.IamRoleArn,
 				IsMeta:  v.IsMetaInstanceProfile,
-			})
+			}
+			if data.Filter != (instanceProfileFilter{}) {
+				re := regexp.MustCompile(data.Filter.Pattern)
+				if !matchesIP(&ipData, re, data.Filter.Name) {
+					continue
+				}
+			}
+			InstanceProfilesData = append(InstanceProfilesData, ipData)
 		}
+
+		data.InstanceProfiles = InstanceProfilesData
 		return nil
 	})
 }
--- aws/data_instance_profiles_test.go
+++ aws/data_instance_profiles_test.go
@@ -7,31 +7,38 @@ import (
 	"github.com/databricks/terraform-provider-databricks/qa"
 )
 
+var testResponse compute.ListInstanceProfilesResponse = compute.ListInstanceProfilesResponse{
+	InstanceProfiles: []compute.InstanceProfile{
+		{
+			IamRoleArn:            "arn:aws:iam::123456789012:role/S3Access",
+			InstanceProfileArn:    "arn:aws:iam::123456789012:instance-profile/S3Access",
+			IsMetaInstanceProfile: true,
+		},
+		{
+			IamRoleArn:            "arn:aws:iam::123456789098:role/KMSAccess",
+			InstanceProfileArn:    "arn:aws:iam::123456789098:instance-profile/KMSAccess",
+			IsMetaInstanceProfile: false,
+		},
+		{
+			InstanceProfileArn:    "arn:aws:iam::123456789098:instance-profile/different",
+			IamRoleArn:            "arn:aws:iam::123456789098:role/value",
+			IsMetaInstanceProfile: false,
+		},
+		{
+			IamRoleArn:            "arn:aws:iam::123456789098:role/Accesses",
+			InstanceProfileArn:    "arn:aws:iam::123456789098:instance-profile/Accesses",
+			IsMetaInstanceProfile: false,
+		},
+	},
+}
+
 func TestInstanceProfilesData(t *testing.T) {
 	qa.ResourceFixture{
 		Fixtures: []qa.HTTPFixture{
 			{
 				Method:   "GET",
 				Resource: "/api/2.0/instance-profiles/list",
-				Response: compute.ListInstanceProfilesResponse{
-					InstanceProfiles: []compute.InstanceProfile{
-						{
-							IamRoleArn:            "arn:aws:iam::123456789012:role/S3Access",
-							InstanceProfileArn:    "arn:aws:iam::123456789012:instance-profile/S3Access",
-							IsMetaInstanceProfile: true,
-						},
-						{
-							IamRoleArn:            "arn:aws:iam::123456789098:role/KMSAccess",
-							InstanceProfileArn:    "arn:aws:iam::123456789098:instance-profile/KMSAccess",
-							IsMetaInstanceProfile: false,
-						},
-						{
-							InstanceProfileArn:    "arn:aws:iam::123456789098:instance-profile/different",
-							IamRoleArn:            "arn:aws:iam::123456789098:role/value",
-							IsMetaInstanceProfile: false,
-						},
-					},
-				},
+				Response: testResponse,
 			},
 		},
 		Resource:    DataSourceInstanceProfiles(),
@@ -56,6 +63,12 @@ func TestInstanceProfilesData(t *testing.T) {
 				"name":     "different",
 				"arn":      "arn:aws:iam::123456789098:instance-profile/different",
 				"role_arn": "arn:aws:iam::123456789098:role/value",
+				"is_meta":  false,
+			},
+			map[string]interface{}{
+				"name":     "Accesses",
+				"arn":      "arn:aws:iam::123456789098:instance-profile/Accesses",
+				"role_arn": "arn:aws:iam::123456789098:role/Accesses",
 				"is_meta":  false,
 			},
 		},
@@ -124,4 +137,324 @@ func TestInstanceProfilesDataDuplicate(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestInstanceProfilesDataFilterContains(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/instance-profiles/list",
+				Response: testResponse,
+			},
+		},
+		Resource:    DataSourceInstanceProfiles(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `
+		filter {
+			name = "name"
+			pattern = "Access"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{
+		"instance_profiles": []interface{}{
+			map[string]interface{}{
+				"name":     "S3Access",
+				"arn":      "arn:aws:iam::123456789012:instance-profile/S3Access",
+				"role_arn": "arn:aws:iam::123456789012:role/S3Access",
+				"is_meta":  true,
+			},
+			map[string]interface{}{
+				"name":     "KMSAccess",
+				"arn":      "arn:aws:iam::123456789098:instance-profile/KMSAccess",
+				"role_arn": "arn:aws:iam::123456789098:role/KMSAccess",
+				"is_meta":  false,
+			},
+			map[string]interface{}{
+				"name":     "Accesses",
+				"arn":      "arn:aws:iam::123456789098:instance-profile/Accesses",
+				"role_arn": "arn:aws:iam::123456789098:role/Accesses",
+				"is_meta":  false,
+			},
+		},
+	})
+}
+
+func TestInstanceProfilesDataFilterEqualsEmpty(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/instance-profiles/list",
+				Response: testResponse,
+			},
+		},
+		Resource:    DataSourceInstanceProfiles(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `
+		filter {
+			name = "name"
+			pattern = "^Access$"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{
+		"instance_profiles": []interface{}{},
+	})
+}
+
+func TestInstanceProfilesDataFilterEquals(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/instance-profiles/list",
+				Response: testResponse,
+			},
+		},
+		Resource:    DataSourceInstanceProfiles(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `
+		filter {
+			name = "name"
+			pattern = "^KMSAccess$"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{
+		"instance_profiles": []interface{}{
+			map[string]interface{}{
+				"name":     "KMSAccess",
+				"arn":      "arn:aws:iam::123456789098:instance-profile/KMSAccess",
+				"role_arn": "arn:aws:iam::123456789098:role/KMSAccess",
+				"is_meta":  false,
+			},
+		},
+	})
+}
+
+func TestInstanceProfilesDataFilterCase(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/instance-profiles/list",
+				Response: testResponse,
+			},
+		},
+		Resource:    DataSourceInstanceProfiles(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `
+		filter {
+			name = "role_arn"
+			pattern = "kms"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{
+		"instance_profiles": []interface{}{},
+	})
+}
+
+func TestInstanceProfilesDataFilterEndsWith(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/instance-profiles/list",
+				Response: testResponse,
+			},
+		},
+		Resource:    DataSourceInstanceProfiles(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `
+		filter {
+			name = "name"
+			pattern = "Access$"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{
+		"instance_profiles": []interface{}{
+			map[string]interface{}{
+				"name":     "S3Access",
+				"arn":      "arn:aws:iam::123456789012:instance-profile/S3Access",
+				"role_arn": "arn:aws:iam::123456789012:role/S3Access",
+				"is_meta":  true,
+			},
+			map[string]interface{}{
+				"name":     "KMSAccess",
+				"arn":      "arn:aws:iam::123456789098:instance-profile/KMSAccess",
+				"role_arn": "arn:aws:iam::123456789098:role/KMSAccess",
+				"is_meta":  false,
+			},
+		},
+	})
+}
+
+func TestInstanceProfilesDataFilterName(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/instance-profiles/list",
+				Response: testResponse,
+			},
+		},
+		Resource:    DataSourceInstanceProfiles(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `
+		filter {
+			name = "name"
+			pattern = "Access$"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{
+		"instance_profiles": []interface{}{
+			map[string]interface{}{
+				"name":     "S3Access",
+				"arn":      "arn:aws:iam::123456789012:instance-profile/S3Access",
+				"role_arn": "arn:aws:iam::123456789012:role/S3Access",
+				"is_meta":  true,
+			},
+			map[string]interface{}{
+				"name":     "KMSAccess",
+				"arn":      "arn:aws:iam::123456789098:instance-profile/KMSAccess",
+				"role_arn": "arn:aws:iam::123456789098:role/KMSAccess",
+				"is_meta":  false,
+			},
+		},
+	})
+}
+func TestInstanceProfilesDataFilterArn(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/instance-profiles/list",
+				Response: testResponse,
+			},
+		},
+		Resource:    DataSourceInstanceProfiles(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `
+		filter {
+			name = "arn"
+			pattern = "arn:aws:iam::123456789098:instance-profile/different"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{
+		"instance_profiles": []interface{}{
+			map[string]interface{}{
+				"name":     "different",
+				"arn":      "arn:aws:iam::123456789098:instance-profile/different",
+				"role_arn": "arn:aws:iam::123456789098:role/value",
+				"is_meta":  false,
+			},
+		},
+	})
+}
+
+func TestInstanceProfilesDataFilterRoleArn(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/instance-profiles/list",
+				Response: testResponse,
+			},
+		},
+		Resource:    DataSourceInstanceProfiles(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `
+		filter {
+			name = "role_arn"
+			pattern = "KMS"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{
+		"instance_profiles": []interface{}{
+			map[string]interface{}{
+				"name":     "KMSAccess",
+				"arn":      "arn:aws:iam::123456789098:instance-profile/KMSAccess",
+				"role_arn": "arn:aws:iam::123456789098:role/KMSAccess",
+				"is_meta":  false,
+			},
+		},
+	})
+}
+
+func TestInstanceProfilesDataFilterIsMeta(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/instance-profiles/list",
+				Response: testResponse,
+			},
+		},
+		Resource:    DataSourceInstanceProfiles(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `
+		filter {
+			name = "is_meta"
+			pattern = "true"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{
+		"instance_profiles": []interface{}{
+			map[string]interface{}{
+				"name":     "S3Access",
+				"arn":      "arn:aws:iam::123456789012:instance-profile/S3Access",
+				"role_arn": "arn:aws:iam::123456789012:role/S3Access",
+				"is_meta":  true,
+			},
+		},
+	})
+}
+
+func TestInstanceProfilesDataFilterBadName(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/instance-profiles/list",
+				Response: testResponse,
+			},
+		},
+		Resource:    DataSourceInstanceProfiles(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `
+		filter {
+			name = "does_not_exist"
+			pattern = "value"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{})
+}
+
+func TestInstanceProfilesDataFilterNameOnly(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/instance-profiles/list",
+				Response: testResponse,
+			},
+		},
+		Resource:    DataSourceInstanceProfiles(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `
+		filter {
+			pattern = "does_not_exist"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{})
 }
